3099846bb7f2f542ad76d7c2faa5730905518966
Taher Najim <taher.najim@yahoo.com>
2024-07-17 18:39:02 +0800

    imported hanken grotesk google font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors: {
         "black": "#060606"
+      },
+      fontFamily: {
+        "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
       }
     },
   },
