2b3cec26b4808478bcac414ac216002c867d0514
Taher Najim <taher.najim@yahoo.com>
2024-07-17 18:39:49 +0800

    added job-card-wide component

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+  <div>
+    <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/90/90" alt="" class="rounded-xl">
+  </div>
+
+  <div class="flex-1">
+    <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+    <h3 class="font-bold text-lg">Video Producer</h3>
+
+    <p class="text-sm text-gray-400">Full Time - From $60,000</p>
+  </div>
+
+  <div>
+    <x-tag>Tag</x-tag>
+    <x-tag>Tag</x-tag>
+    <x-tag>Tag</x-tag>
+  </div>
+
+</div>
