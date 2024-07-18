f1cceec19b2a1486c1a3056e14a60c42d1f473b7
Taher Najim <taher.najim@yahoo.com>
2024-07-18 18:18:15 +0800

    moved employer logo to its own component

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,14 +1,14 @@
 <div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
   <div>
-    <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/90/90" alt="" class="rounded-xl">
+    <x-employer-logo />
   </div>
 
-  <div class="flex-1">
+  <div class="flex-1 flex flex-col">
     <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
 
-    <h3 class="font-bold text-lg">Video Producer</h3>
+    <h3 class="font-bold text-xl">Video Producer</h3>
 
-    <p class="text-sm text-gray-400">Full Time - From $60,000</p>
+    <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
   </div>
 
   <div>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,17 +2,17 @@
   <div class="self-start text-sm">Laracasts</div>
 
   <div class="py-8 font-bold">
-      <h3>Video Producer</h3>
-      <p>Full Time - From $60,000</p>
+    <h3>Video Producer</h3>
+    <p>Full Time - From $60,000</p>
   </div>
 
   <div class="flex justify-between items-center mt-auto">
-      <div>
-          <x-tag>Tag</x-tag>
-          <x-tag>Tag</x-tag>
-          <x-tag>Tag</x-tag>
-      </div>
+    <div>
+      <x-tag>Tag</x-tag>
+      <x-tag>Tag</x-tag>
+      <x-tag>Tag</x-tag>
+    </div>
 
-      <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    <x-employer-logo :width="42" />
   </div>
 </div>
