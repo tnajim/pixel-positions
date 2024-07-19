e60da6ed0bca6e2d203507192a0cb336baa6cf16
Taher Najim <taher.najim@yahoo.com>
2024-07-19 17:12:11 +0800

    created panel component and tag size base/small

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
   <div>
     <x-employer-logo />
   </div>
@@ -6,7 +6,7 @@
   <div class="flex-1 flex flex-col">
     <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
 
-    <h3 class="font-bold text-xl">Video Producer</h3>
+    <h3 class="font-bold text-xl group-hover:text-blue-700 transition-colors duration-300">Video Producer</h3>
 
     <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
   </div>
@@ -17,4 +17,4 @@
     <x-tag>Tag</x-tag>
   </div>
 
-</div>
+</x-panel>
