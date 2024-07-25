24b1385b3918abf49390ed7739467192c540ca26
Taher Najim <taher.najim@yahoo.com>
2024-07-25 19:03:54 +0800

    created simple job-employer instance relationship unit test

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Models\Employer;
+use App\Models\Job;
 
 it('belongs to an employer', function () {
-    // Arrange
-    // Act
-    // Asser
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
+
 });
