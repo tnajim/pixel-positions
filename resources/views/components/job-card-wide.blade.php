f26312aed7d2ce6e852d338662b56ee8d64bed1a
Taher Najim <taher.najim@yahoo.com>
2024-08-09 17:02:45 +0800

    replaced dummy data with data from the db

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -6,16 +6,16 @@
   </div>
 
   <div class="flex-1 flex flex-col">
-    <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+    <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-    <h3 class="font-bold text-xl group-hover:text-blue-700 transition-colors duration-300">Video Producer</h3>
+    <h3 class="font-bold text-xl group-hover:text-blue-700 transition-colors duration-300">{{ $job->title }}</h3>
 
-    <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
   </div>
 
   <div>
     @foreach ($job->tags as $tag)
-      <x-tag :$tag>Backend</x-tag>
+      <x-tag :$tag />
     @endforeach
   </div>
 
