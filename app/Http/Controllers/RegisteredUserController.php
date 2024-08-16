f1cf6222b0d2eb5675286b0a39e896ab84b0d662
Taher Najim <taher.najim@yahoo.com>
2024-08-16 17:40:00 +0800

    created register page and completed the controller for it

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,63 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    return view('auth.register');
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $userAttributes = $request->validate([
+      'name' => ['required'],
+      'email' => ['required', 'email', 'unique:users,email'],
+      'password' => ['required', 'confirmed', Password::min(6)],
+    ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    $employerAttributes = $request->validate([
+      'employer' => ['required'],
+      'logo' => ['required', File::types(['png', 'jpg', 'webp'])],
+    ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+    $user = User::create($userAttributes);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+    $logoPath = $request->logo->store('logos');
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+    $user->employer()->create([
+      'name' => $employerAttributes['employer'],
+      'logo' => $logoPath
+    ]);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    Auth::login($user);
+
+    return redirect('/');
+  }
 }
