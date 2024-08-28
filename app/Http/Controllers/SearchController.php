26bee95c50db13107c65c2f079493f002fd20a7b
Taher Najim <taher.najim@yahoo.com>
2024-08-28 15:48:18 +0800

    implemented a job search feature

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function __invoke()
     {
-        dd(request('q'));
+        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
+
+        return view('results', ['jobs' => $jobs]);
     }
 }
