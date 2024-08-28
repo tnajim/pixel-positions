f6416f38b6d827edfebcf6434ce8dc12c79ed2a5
Taher Najim <taher.najim@yahoo.com>
2024-08-28 15:58:08 +0800

    initialized and imported TagController

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    public function __invoke()
+    {
+        // find jobs for this tag
+
+        // return view('results', ['jobs' => $jobs]);
+    }
+}
