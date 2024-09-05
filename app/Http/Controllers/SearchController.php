431741fd49880253a1e2b66a96c1040b7d5a596a
Taher Najim <taher.najim@yahoo.com>
2024-09-05 17:51:59 +0800

    optimized query using eager loading

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function __invoke()
-    {
-        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
+  public function __invoke()
+  {
+    $jobs = Job::query()
+      ->with(['employer', 'tags'])
+      ->where('title', 'LIKE', '%' . request('q') . '%')
+      ->get();
 
-        return view('results', ['jobs' => $jobs]);
-    }
+    return view('results', ['jobs' => $jobs]);
+  }
 }
